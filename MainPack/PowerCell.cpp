80171c41e68d395bdaca18d7bffd143578c167c1
Jordan Byron <jordan.byron@gmail.com>
2026-09-20 12:14:39 -0400

    refactor: Make PowerCell own its animation state (#9)

--- MainPack/PowerCell.cpp
+++ MainPack/PowerCell.cpp
@@ -7,25 +7,17 @@
 const int powercellLedCount = 14;    // total number of led's in the animation
 const int powercellIndexOffset = 0;  // first led offset into the led chain for the animation
 
-unsigned long prevPwrBootMillis = 0;         // the last time we changed a powercell light in the boot sequence
 const unsigned long pwr_boot_interval = 30;  // interval at which to cycle lights (milliseconds). Adjust this if
 
-// LED tracking variables
 const int powerSeqTotal = powercellLedCount;                         // total number of led's for powercell 0 based
-int powerSeqNum = powercellIndexOffset;                              // current running powercell sequence led
-int powerShutdownSeqNum = powercellLedCount - powercellIndexOffset;  // shutdown sequence counts down
-
-// animation level trackers for the boot and shutdown
-int currentBootLevel = powercellIndexOffset;                       // current powercell boot level sequence led
-int currentLightLevel = powercellLedCount - powercellIndexOffset;  // current powercell boot light sequence led
-
-bool powerBoot = false;
-
-FireTimer offTimer;
 
 PowerCell::PowerCell(uint16_t numberOfLeds, int16_t pin) {
   _numberOfLeds = numberOfLeds;
   _pin = pin;
+  _powerSeqNum = powercellIndexOffset;
+  _powerShutdownSeqNum = powercellLedCount - powercellIndexOffset;
+  _currentBootLevel = powercellIndexOffset;
+  _currentLightLevel = powercellLedCount - powercellIndexOffset;
 }
 
 void PowerCell::setup() {
@@ -39,36 +31,36 @@ void PowerCell::setup() {
 void PowerCell::boot(unsigned long currentMillis) {
   bool doUpdate = false;
 
-  if (powerBoot == true) { 
+  if (this->_powerBoot == true) { 
     this->idle(currentMillis, 1000);
     return;
   }
 
-  if ((unsigned long)(currentMillis - prevPwrBootMillis) >= pwr_boot_interval) {
+  if ((unsigned long)(currentMillis - this->_prevPwrBootMillis) >= pwr_boot_interval) {
     // save the last time you blinked the LED
-    prevPwrBootMillis = currentMillis;
+    this->_prevPwrBootMillis = currentMillis;
 
     // START POWERCELL
-    if (currentBootLevel != powerSeqTotal) {
-      if (currentBootLevel == currentLightLevel) {
-        if (currentLightLevel + 1 <= powerSeqTotal) {
-          this->_lights->setPixelColor(currentLightLevel + 1, 0);
+    if (this->_currentBootLevel != powerSeqTotal) {
+      if (this->_currentBootLevel == this->_currentLightLevel) {
+        if (this->_currentLightLevel + 1 <= powerSeqTotal) {
+          this->_lights->setPixelColor(this->_currentLightLevel + 1, 0);
         }
-        this->_lights->setPixelColor(currentBootLevel, this->_lights->Color(0, 0, 255));
-        currentLightLevel = powerSeqTotal;
-        currentBootLevel++;
+        this->_lights->setPixelColor(this->_currentBootLevel, this->_lights->Color(0, 0, 255));
+        this->_currentLightLevel = powerSeqTotal;
+        this->_currentBootLevel++;
       } else {
-        if (currentLightLevel + 1 <= powerSeqTotal) {
-          this->_lights->setPixelColor(currentLightLevel + 1, 0);
+        if (this->_currentLightLevel + 1 <= powerSeqTotal) {
+          this->_lights->setPixelColor(this->_currentLightLevel + 1, 0);
         }
-        this->_lights->setPixelColor(currentLightLevel, this->_lights->Color(0, 0, 255));
-        currentLightLevel--;
+        this->_lights->setPixelColor(this->_currentLightLevel, this->_lights->Color(0, 0, 255));
+        this->_currentLightLevel--;
       }
       doUpdate = true;
     } else {
-      powerBoot = true;
-      currentBootLevel = powercellIndexOffset;
-      currentLightLevel = powercellLedCount - powercellIndexOffset;
+      this->_powerBoot = true;
+      this->_currentBootLevel = powercellIndexOffset;
+      this->_currentLightLevel = powercellLedCount - powercellIndexOffset;
     }
     // END POWERCELL
   }
@@ -78,28 +70,26 @@ void PowerCell::boot(unsigned long currentMillis) {
   }
 }
 
-unsigned long prevPwrMillis = 0;  // last time we changed a powercell light in the idle sequence
-
 void PowerCell::idle(unsigned long currentMillis, unsigned long anispeed) {
   bool doUpdate = false;
 
   // START POWERCELL
-  if ((unsigned long)(currentMillis - prevPwrMillis) >= anispeed) {
+  if ((unsigned long)(currentMillis - this->_prevPwrMillis) >= anispeed) {
     // save the last time you blinked the LED
-    prevPwrMillis = currentMillis;
+    this->_prevPwrMillis = currentMillis;
 
     for (int i = powercellIndexOffset; i <= powerSeqTotal; i++) {
-      if (i <= powerSeqNum) {
+      if (i <= this->_powerSeqNum) {
         this->_lights->setPixelColor(i, this->_lights->Color(0, 0, 150));
       } else {
         this->_lights->setPixelColor(i, 0);
       }
     }
 
-    if (powerSeqNum <= powerSeqTotal) {
-      powerSeqNum++;
+    if (this->_powerSeqNum <= powerSeqTotal) {
+      this->_powerSeqNum++;
     } else {
-      powerSeqNum = powercellIndexOffset;
+      this->_powerSeqNum = powercellIndexOffset;
     }
 
     doUpdate = true;
@@ -112,20 +102,18 @@ void PowerCell::idle(unsigned long currentMillis, unsigned long anispeed) {
   }
 }
 
-bool animationComplete = false;
-
 // FIXME: I think this is actually Overload
 void PowerCell::off(bool start) {
   if(start) { 
-    animationComplete = false;
-    offTimer.begin(175);
+    this->_animationComplete = false;
+    this->_offTimer.begin(175);
   }
 
-  if(animationComplete) return;
+  if(this->_animationComplete) return;
 
-  if (offTimer.fire()) {
+  if (this->_offTimer.fire()) {
     for (int i = powerSeqTotal; i >= powercellIndexOffset; i--) {
-      if (i <= powerShutdownSeqNum) {
+      if (i <= this->_powerShutdownSeqNum) {
         this->_lights->setPixelColor(i, this->_lights->Color(0, 0, 150));
       } else {
         this->_lights->setPixelColor(i, 0);
@@ -134,23 +122,22 @@ void PowerCell::off(bool start) {
 
     this->_lights->show();
 
-    if (powerShutdownSeqNum >= powercellIndexOffset) {
-      powerShutdownSeqNum--;
+    if (this->_powerShutdownSeqNum >= powercellIndexOffset) {
+      this->_powerShutdownSeqNum--;
     } else {
-      powerShutdownSeqNum = powercellLedCount - powercellIndexOffset;
-      animationComplete = true;
+      this->_powerShutdownSeqNum = powercellLedCount - powercellIndexOffset;
+      this->_animationComplete = true;
     }
   }
 }
 
-
 void PowerCell::clear() {
-  powerSeqNum = powercellIndexOffset;
-  powerShutdownSeqNum = powercellLedCount - powercellIndexOffset;
-  currentLightLevel = powercellLedCount;
-  currentBootLevel = powercellIndexOffset;
-  powerBoot = false;
-  animationComplete = false;
+  this->_powerSeqNum = powercellIndexOffset;
+  this->_powerShutdownSeqNum = powercellLedCount - powercellIndexOffset;
+  this->_currentLightLevel = powercellLedCount;
+  this->_currentBootLevel = powercellIndexOffset;
+  this->_powerBoot = false;
+  this->_animationComplete = false;
 
   this->_lights->clear();
   this->_lights->show();
